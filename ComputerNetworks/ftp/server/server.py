e03b342ff09c6ad6cbfad6e6d600cfd6634b6e0e
Seda <sedan.taskan@gmail.com>
2024-05-07 23:17:55 +0300

    chore: Refactor FTP server code for better organization and readability

--- ComputerNetworks/ftp/server/server.py
+++ ComputerNetworks/ftp/server/server.py
@@ -2,20 +2,16 @@ from pyftpdlib.authorizers import DummyAuthorizer
 from pyftpdlib.handlers import FTPHandler
 from pyftpdlib.servers import FTPServer
 
-# create a user for login
-authorizer = DummyAuthorizer()
-authorizer.add_user("seda", "6653", "files", perm="elradfmw")
+def runServer():
+    authorizer = DummyAuthorizer()
+    authorizer.add_user("seda", "6653", ".", perm="elradfmw")
 
-# Instantiate FTP handler class
-handler = FTPHandler
-handler.authorizer = authorizer
+    # Instantiate FTP handler class
+    handler = FTPHandler
+    handler.authorizer = authorizer
 
-# Instantiate FTP server class and listen on 0.0.0.0:21
-server = FTPServer(("0.0.0.0", 21), handler)
+    # Instantiate FTP server class and listen on 0.0.0.0:21
+    server = FTPServer(("127.0.0.1", 21), handler)
+    server.serve_forever()
 
-# set a limit for connections
-server.max_cons = 256
-server.max_cons_per_ip = 5
-
-# start FTP server
-server.serve_forever()
+runServer()
